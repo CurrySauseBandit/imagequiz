c13a41689ac1fcb88f859a9d8b9b23c8e7583335
TheCurrySauseBandit <elementalspear@gmail.com>
2021-10-01 01:38:31 -0700

    setup

---
+++ imagequiz/src/components/Home.js
@@ -0,0 +1,19 @@
+import flowers from "./Data";
+const Home = () => {
+    return(
+
+        <html>
+            <head>
+                <title> My home page</title>
+            </head>
+            <body>
+                <h1> My home page</h1>
+                <p> Hello from home page! </p>
+            </body>
+        </html>
+        
+
+    );
+}
+
+export default Home;
---
+++ imagequiz/src/components/Register.js
@@ -0,0 +1,9 @@
+
+
+const Register = () => {
+    return(
+        <p>This is the register page!</p>
+    );
+}
+
+export default Register;
